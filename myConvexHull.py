cc69f7fb414df10921bb9f9d5166e8154ad19920
adeliaaaa <monicadelia88@gmail.com>
2022-02-27 02:19:31 +0700

    ad find fartest point

--- myConvexHull.py
+++ myConvexHull.py
@@ -11,7 +11,7 @@ def ConvexHullMonic(bucket):
     #max_index_col = np.argmax(bucket, axis=0)
     #min_index_col = np.argmin(bucket, axis=0)
     
-    solution = np.array([[0,0]])
+    solution = np.array([[0.0,0.0]])
 
     #solution[0] = max_index_col
     #solution = np.vstack((solution, min_index_col))
@@ -21,15 +21,9 @@ def ConvexHullMonic(bucket):
     print(sort[len(sort)-1])
 
     solution[0] = sort[0]
-    solution = np.vstack((solution, bucket[len(sort)-1]))
+    solution = np.vstack((solution, sort[len(sort)-1]))
     
     print(solution)
-
-    s1 = np.array([[1, 2]])
-    s1 = np.delete(s1, 0, axis=0)
-    s2 = np.array([[1, 2]])
-    s2 = np.delete(s2, 0, axis=0)
-
 
 
     #print(bucket[solution[0,0],0])
@@ -41,9 +35,12 @@ def ConvexHullMonic(bucket):
     #print(bucket[solution[1,1],0])
     #print(bucket[solution[1,1],1])
 
-
-
     matriks = np.array([[solution[0,0], solution[0,1], 1], [solution[1,0], solution[1,1], 1], [0,0,1]])
+    s1 = np.array([[1.0, 2.0]])
+    s1 = np.delete(s1, 0, axis=0)
+
+    s2 = np.array([[1.0, 2.0]])
+    s2 = np.delete(s2, 0, axis=0)
     
 
     for i  in range (len(bucket)):
@@ -56,10 +53,13 @@ def ConvexHullMonic(bucket):
             else:
                 s2 = np.vstack((s2, bucket[i]))
 
-    print("s1")
-    print(s1)
-    print("s2")
-    print(s2)
+    findHull(s1, solution[0], solution[1])
+    findHull(s1, solution[1], solution[0])
+
+    #print("s1")
+    #print(s1)
+    #print("s2")
+    #print(s2)
 
 
     #np.append([[1, 2, 3], [4, 5, 6]], [[7, 8, 9]], axis=0)
@@ -70,6 +70,32 @@ def ConvexHullMonic(bucket):
     #print(result[1])
     #print(bucket.index(result[0]))
     return solution
+
+
+def findHull(S,A,B):
+    print("Masuk sini---------------------------------")
+    print(A[0])
+    print(A[1])
+    print(B[0])
+    print(B[1])
+    print("Masuk sini---------------------------------")
+    if(S.size == 0):
+        print('CEKTES---------------')
+    else:
+        # Find Orthogonally farthest point from AB
+        fartest = S[0]
+        tempDistance = 0
+        for i in range (len(S)):
+            sisiA = (((B[1] - A[1])**2) + ((B[0]-A[0])**2))**(1/2)
+            sisiB = (((S[1] - A[1])**2) + ((S[0]-A[0])**2))**(1/2)
+            sisiC = (((B[1] - S[1])**2) + ((B[0]-S[0])**2))**(1/2)
+            setKel = (sisiA + sisiB + sisiC)/2
+            jarak = (2/sisiA)*((setKel*(setKel-sisiA)*(setKel-sisiB)*(setKel-sisiC))**(1/2))
+            if(jarak > tempDistance):
+                tempDistance = jarak
+                fartest = S[i]
+        
+        
 
 
 '''
@@ -83,9 +109,7 @@ Solution ← AB followed by BA
 
 Call FindHull(S1, A, B)
 Call FindHull(S2, B, A)
-
-
-
+---------
 Algorithm FindHull(P, A, B)
 
 if isEmpty(P) then
